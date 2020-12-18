ab1a535c2f2f878d0f0eac283bf49dc0b893be41
albert-du <52804499+albert-du@users.noreply.github.com>
2020-12-18 13:31:59 -0800

    Release Action Workflow
    Workflow to create a release

--- modules/ModChangeCollator.fsx
+++ modules/ModChangeCollator.fsx
@@ -1,34 +1,71 @@
-// This file runs prior to releasing
-// Purpose: find mod changes from last time 
+// For Github Action
+// find mod changes from last time 
 // saves old list in 'modListPrevious.txt' for comparison
+// creates temp file 'changelog.txt'
 open System.IO
+open System.Linq;
 
 let currentMods = 
     Directory.GetFiles("./mods/", "*.jar", SearchOption.AllDirectories)
-    |> Array.map Path.GetFileName
-    |> Array.sort
+    |> Array.toSeq
+    |> Seq.map Path.GetFileName
+    |> Seq.sort
 
-let priviousMods = 
-    File.ReadLines("./modules/modListPrevious.txt") |> Seq.toArray
+let previousMods = 
+    File.ReadLines("./modules/modListPrevious.txt") |> seq
 
-let cleanModList (mods: string array) =
+let cleanModName (modName:string) =
+    match modName.Contains('-') with
+    | true -> modName.Split('-').[0]
+    | false -> modName
+
+let cleanModList (mods: string seq) =
     mods
-    |> Array.map (fun modName -> 
-        match modName.Contains('-') with
-        | true -> modName.Split('-').[0]
-        | false -> modName)
+    |> Seq.map cleanModName
 
 let cleanedCurrentMods = cleanModList currentMods
-let cleanedPreviousMods = cleanModList priviousMods
+let cleanedPreviousMods = cleanModList previousMods
 // additions
-printfn "%A" cleanedCurrentMods 
-    
+let addedMods = 
+    cleanedCurrentMods |> fun i -> i.Except(cleanedPreviousMods)
+
 // removals
+let removedMods = 
+    cleanedPreviousMods |> fun i -> i.Except(cleanedCurrentMods)
 
 // version changes
+let versionChanges =
+    let filteredModsCurrent =
+        currentMods
+        |> Seq.filter (fun i -> Seq.contains (cleanModName i) cleanedPreviousMods )
+    let filteredModsPrevious =
+        previousMods
+        |> Seq.filter (fun i -> Seq.contains (cleanModName i) cleanedCurrentMods )
+    Seq.map2 (fun o n -> (o,n))  filteredModsPrevious filteredModsCurrent //pair up
+    |> Seq.filter(fun (o,n) ->  o<>n)
+    |> Seq.map( fun (o , n) -> sprintf "%s -> %s" o n)
 
-// file output, remember to delete in action
+// file output
+do
+    use streamWriter = new StreamWriter("./changelog.md", false)
+    streamWriter.WriteLine("## Changes:")
+    streamWriter.WriteLine("---")
 
+    if Seq.length addedMods > 0 then
+        streamWriter.WriteLine("#### Additions:")
+        Seq.iter(fun i -> 
+            sprintf "* %s" i 
+            |> streamWriter.WriteLine ) addedMods
+    if Seq.length removedMods > 0 then
+        streamWriter.WriteLine("#### Removals:")
+        Seq.iter(fun i -> 
+            sprintf "* %s" i 
+            |> streamWriter.WriteLine ) removedMods
+    if Seq.length versionChanges > 0 then
+        streamWriter.WriteLine("#### Updates:")
+        Seq.iter(fun i -> 
+            sprintf "* %s" i 
+            |> streamWriter.WriteLine ) versionChanges
 
 // update prev list
 File.WriteAllLines("./modules/modListPrevious.txt", currentMods)
