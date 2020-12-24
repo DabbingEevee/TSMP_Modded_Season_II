037497fc3e50dc48ac6140aec227c390bcf64abc
albert-du <52804499+albert-du@users.noreply.github.com>
2020-12-23 22:52:07 -0800

    Add workflow and script for modlist generation
    Adds Actions workflow for generating markdown mod list file
    
    gets name, author, and url from mcmod.info file inside mod jar files

---
+++ modules/ModListUpdate.fsx
@@ -0,0 +1,59 @@
+// Find all mod name and versions, write to ModList.md
+open System
+open System.Text.Json
+open System.IO
+open System.IO.Compression
+open System.Linq
+open System.Globalization
+open System.Text.RegularExpressions
+
+
+let readModInfo (text:string) =
+    let sanitizedText = 
+        text.Split('\n')
+        |> Array.filter ( fun x -> not <| (x.Contains("\"dependencies\":") && (x.Contains("tconstruct") || x.Contains("minerva") ) ))
+        |> String.Concat
+        |> fun i -> i.Replace("\n"," ")
+    let mutable jsonOptions = JsonDocumentOptions()
+    jsonOptions.AllowTrailingCommas <- true
+    let rootElement = JsonDocument.Parse(sanitizedText, jsonOptions ).RootElement.[0] 
+    let name =
+        try
+            "### " + rootElement.GetProperty("name").GetRawText().Replace("\"","") + "\n"
+        with 
+        | :? Collections.Generic.KeyNotFoundException -> "### Unknown Name\n"
+        | ex -> ex.Message
+    let url = 
+        try
+            "<" + rootElement.GetProperty("url").GetRawText().Replace("\"","").Trim() + ">\n"
+        with 
+        | :? Collections.Generic.KeyNotFoundException -> ""
+    let authorList = 
+        try
+            "*" + rootElement.GetProperty("authorList").GetRawText().Replace("\"","") .Replace("]","").Replace("[","").Trim() + "*\n"
+        with 
+        | :? Collections.Generic.KeyNotFoundException -> ""
+    name + url + authorList + "***\n"
+
+Directory.GetFiles("./mods/", "*.jar", SearchOption.AllDirectories)
+|> Array.toSeq
+|> Seq.sort
+|> Seq.choose ( fun path -> 
+    use archive = ZipFile.OpenRead(path)
+    printfn "%s" path
+    try
+        archive.Entries.First(fun entry -> entry.FullName.ToLower() = "mcmod.info")
+        |> fun stream ->
+            use fileStream = new StreamReader(stream.Open())
+            fileStream.ReadToEnd()
+            |> readModInfo
+        |> Some 
+    with 
+    | :? InvalidOperationException -> 
+        printfn "something caught"
+        None)
+|> Seq.toList
+|> fun i -> "## Mod List\n" :: i
+|> List.toArray
+|> String.Concat
+|> fun i -> File.WriteAllText ("./ModList.md", i)
